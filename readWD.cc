ac61c022647ffadf4d863ea7efb215e850f05819
Matteo Brini <brinimatteo@gmail.com>
2022-12-23 12:46:17 +0100

    DAQFile::operator bool uses new simplified logic, specialized DAQFile::Read for vector<float>, fixed CreateBoard and CreateChannel.

--- readWD.cc
+++ readWD.cc
@@ -40,66 +40,126 @@ ostream &operator<<(ostream &o, const EventHeader &eh) // cout << EventHeader
     return o;
 }
 
-void DAQFile::Initialise(DAQEvent &event)
-{
-    DAQFile &file = *this;
-    TAG tag;
-    float times[SAMPLES_PER_WAVEFORM];
-
-    file >> tag >> tag; // DRSx - TIME
-    while (file >> tag) // B#?
-    {
-        cout << tag << endl; // print --> B#?
-        event.CreateBoard(tag);
-        while (file >> tag) // C001
-        {
-            cout << tag << endl; // print --> C001
-            file.Read(times);
-            event.Time(times);
-        }
-        file.ResetTag();
-    }
-    cout << "Last tag found: " << tag << endl;
-    file.ResetTag();
-    return;
-}
-
 DAQFile::operator bool()
 {
+    map<char, char> header{{'E', 'B'},{'B', 'C'},{'C','E'}};
     if (in_.eof())
     {
         return 0;
     }
-    else if (n_ != 'B' and n_ != 'C')
+    else if (n_ != 'B' and n_ != 'C' and n_ != 'E')
     {
+        cerr << "!! Error --> Unexpected tag" << endl;
         return 0;
     }
-    o_ = n_;
-    return 1;
+    else if (n_ == o_) return 1;
+    else if (n_ == header[o_])
+    {
+        if (n_ == 'E' and !initialization_)
+        {
+            cout << "Initialization done --> EHDR next" << endl;
+            initialization_ = 1;
+            o_ = 'B';
+            return 0;
+        }
+        o_ = n_;
+        return 1;
+    }
+    else if (o_ == 'C' and n_ == 'B')
+    {
+        o_ = n_;
+        return 1;
+    }
+    return 0;
 }
 
-void DAQEvent::Time(float *times)
+void DAQEvent::SetTime(const vector<float> &times)
 {
     if (!times_.empty())
     {
-        times_.rbegin()->second.push_back(times);
+        if (!times_.rbegin()->second.empty())
+        {
+            times_.rbegin()->second.rbegin()->second = std::move(times);
+        }
     }
     return;
 }
 
 void DAQEvent::CreateBoard(const TAG &tag)
 {
-    if (strcmp(tag.tag, "EHDR") == 0)
+    if (tag.tag[0] == 'B' and tag.tag[1] == '#')
     {
-        cout << "Initialization done --> EHDR next" << endl;
+        string board{"B#" + to_string(*(short *)(tag.tag + 2))};
+        times_[board] = {};
+        volts_[board] = {};
         return;
     }
-    else if (tag.tag[0] == 'B' and tag.tag[1] == '#')
-    {
-        times_[tag.tag] = {};
-        volts_[tag.tag] = {};
-        return;
-    }
-    cerr << "!! No valid tag passed: expecterd B#?, got " << tag << endl;
+    cerr << "!! Error: no valid tag passed --> expecterd B#?, got " << tag << endl;
     return;
 }
+
+void DAQEvent::CreateChannel(const TAG &tBoard, const TAG &tag)
+{
+    string board{"B#" + to_string(*(short *)(tBoard.tag + 2))};
+    if (times_.find(board) == times_.end())
+    {
+        cerr << "!! Error: invalid board for this channel --> " << board << endl;
+        return;
+    }
+    if (tag.tag[0] == 'C')
+    {
+        string channel{tag.tag};
+        times_[board][channel] = {};
+        volts_[board][channel] = {};
+        return;
+    }
+    cerr << "!! No valid tag passed: expecterd C, got " << tag << endl;
+    return;
+}
+
+void DAQFile::Initialise(DAQEvent &event)
+{
+    if (not in_.is_open())
+    {
+        cerr << "!! Error: file not open --> use DAQFile(filename)" << endl;
+        return;
+    }
+
+    DAQFile &file = *this;
+    TAG bTag, cTag;
+    vector<float> times(SAMPLES_PER_WAVEFORM, -1);
+
+    cout << "Initializing file " << filename_ << endl;
+
+    file >> bTag >> cTag; // DRSx - TIME
+    cout << bTag << endl
+         << cTag << endl;
+    
+    o_ = 'B';
+    initialization_ = 0;
+    while (file >> bTag) // B#?
+    {
+        cout << bTag << ":" << endl; // print --> B#?
+        event.CreateBoard(bTag);
+        while (file >> cTag) // C001
+        {
+            cout << " --> " << cTag << endl; // print --> C001
+            event.CreateChannel(bTag, cTag);
+            file.Read(times);
+            event.SetTime(times);
+        }
+        file.ResetTag();
+    }
+    file.ResetTag();
+    return;
+}
+
+void DAQFile::Read(vector<float> &vec)
+{
+    for (int i = 0; i < vec.size(); ++i)
+    {
+        in_.read((char *)&vec.at(i), sizeof(float));
+    }
+    return;
+}
+

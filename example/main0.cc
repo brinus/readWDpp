56f57e62afcf6bbbfbf5485633d1e13db2e6404c
Matteo Brini <brinimatteo@gmail.com>
2022-12-30 15:28:19 +0100

    Testing multiple file reading

--- example/main0.cc
+++ example/main0.cc
@@ -6,10 +6,17 @@ using namespace std;
 int main(void)
 {
     DAQFiles files({"test/testDRS.dat", "test/testWDB.dat"});
-    for (auto filename : files)
+    for (auto &file : files)
     {
-        DAQFile file(filename);
         DAQEvent event;
-        file.Initialise(event);
+        file->Initialise(event);
+        file->Close();
+    }
+
+    // What happends if I iterate again? It generates an error --> to be solved
+    for (auto &file : files)
+    {
+        DAQEvent event;
+        file->Initialise(event);
     }
 }

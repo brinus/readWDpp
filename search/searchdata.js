4c53478b4605e87413bc3c35568068ee77bea4f5
brinus <brinus@users.noreply.github.com>
2023-01-02 10:51:17 +0000

    deploy: 36797541866b7d57ff45b8b77637746edca59484

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,18 +1,21 @@
 var indexSectionsWithContent =
 {
-  0: "det",
-  1: "det"
+  0: "dert",
+  1: "det",
+  2: "r"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes"
+  1: "classes",
+  2: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes"
+  1: "Classes",
+  2: "Pages"
 };
 

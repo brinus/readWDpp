f6441262995763c6515cd170efed2ed7c5da2d8b
brinus <brinus@users.noreply.github.com>
2023-01-04 16:32:41 +0000

    deploy: b3ae199ce655986cb593a8cca01994d6b8e3e402

--- search/classes_0.js
+++ search/classes_0.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['daqevent_0',['DAQEvent',['../classDAQEvent.html',1,'']]],
   ['daqfile_1',['DAQFile',['../classDAQFile.html',1,'']]],
-  ['daqfiles_2',['DAQFiles',['../classDAQFiles.html',1,'']]],
-  ['drsevent_3',['DRSEvent',['../classDRSEvent.html',1,'']]]
+  ['drsevent_2',['DRSEvent',['../classDRSEvent.html',1,'']]]
 ];

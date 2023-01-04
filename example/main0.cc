26b64b96abb68a032180e422fd09c7f30eeb744f
Matteo Brini <brinimatteo@gmail.com>
2023-01-04 17:31:10 +0100

    minors

--- example/main0.cc
+++ example/main0.cc
@@ -5,18 +5,5 @@ using namespace std;
 
 int main(void)
 {
-    DAQFiles files({"test/testDRS.dat", "test/testWDB.dat"});
-    for (auto &file : files)
-    {
-        DAQEvent event;
-        file->Initialise(event);
-        file->Close();
-    }
-
-    // What happends if I iterate again? It generates an error --> to be solved
-    for (auto &file : files)
-    {
-        DAQEvent event;
-        file->Initialise(event);
-    }
+    // Test something
 }

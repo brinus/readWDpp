c7b10bc7e328ef6971a0f0c4f0a0d0a9b95b9166
Matteo Brini <brinimatteo@gmail.com>
2023-01-09 16:14:33 +0100

    testing GetPedestal()

--- example/main0.cc
+++ example/main0.cc
@@ -1,9 +1,31 @@
 #include "../readWD.hh"
-#include <iostream>
+#include "TH1F.h"
 
 using namespace std;
 
 int main(void)
 {
-    // Test something
+    DAQFile file("test/testDRS.dat");
+    DRSEvent event;
+    TH1F *h1 = new TH1F("Pedestal mean", "h1", 100, -0.002, 0.004);
+    TH1F *h2 = new TH1F("Pedestal std.dev.", "h2", 100, -0.002, 0.004);
+
+    h1->SetLineColor(kRed);
+    h2->SetLineColor(kGreen);
+
+    file.Initialise(event);
+    while(file >> event)
+    {
+        auto pedestal = event.GetChannel("B#2440", "C001").GetPedestal();
+        h1->Fill(pedestal.first);
+        h2->Fill(pedestal.second);
+    }
+
+    h2->Draw();
+    h1->Draw("SAMES");
+}
+
+void main0()
+{
+    main();
 }

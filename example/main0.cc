1b1c8263549058a7cbccb3dfaa94a00e8bae5f53
Matteo Brini <brinimatteo@gmail.com>
2023-01-09 16:38:33 +0100

    documenting the example

--- example/main0.cc
+++ example/main0.cc
@@ -1,3 +1,20 @@
+/*!
+ @example main0.cc
+ @author Matteo Brini (brinimatteo@gmail.com)
+ @brief Example on how to use @ref DAQEvent::GetPedestal()
+ @version 0.1
+ @date 2023-01-09
+ 
+ @copyright Copyright (c) 2023
+ 
+ @details The method @ref DAQFile::operator>> is used to fill at each event the instance of @ref DAQEvent.
+ In the `while` the object `event` is updated everytime with the new event read from the file. The the proper channel is
+ chosen with @ref DAQEvent::GetChannel() and in cascade is used @ref DAQEvent::GetPedestal() that returns an 
+ `std::pair<float, float>`, the two values are the mean and the standard deviation of the pedestal, these values are 
+ used to fill two distinct histograms using ROOT. 
+ 
+ */
+
 #include "../readWD.hh"
 #include "TH1F.h"
 

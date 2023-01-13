7b425397aa4aa08c34d91d9f6f3b7ce9555f551c
Matteo Brini <brinimatteo@gmail.com>
2023-01-13 15:21:29 +0100

    minors

--- example/main0.cc
+++ example/main0.cc
@@ -4,15 +4,15 @@
  @brief Example on how to use @ref DAQEvent::GetPedestal()
  @version 0.1
  @date 2023-01-09
- 
+
  @copyright Copyright (c) 2023
- 
+
  @details The method @ref DAQFile::operator>> is used to fill at each event the instance of @ref DAQEvent.
  In the `while` the object `event` is updated everytime with the new event read from the file. The the proper channel is
- chosen with @ref DAQEvent::GetChannel() and in cascade is used @ref DAQEvent::GetPedestal() that returns an 
- `std::pair<float, float>`, the two values are the mean and the standard deviation of the pedestal, these values are 
- used to fill two distinct histograms using ROOT. 
- 
+ chosen with @ref DAQEvent::GetChannel() and in cascade is used @ref DAQEvent::GetPedestal() that returns an
+ `std::pair<float, float>`, the two values are the mean and the standard deviation of the pedestal, these values are
+ used to fill two distinct histograms using ROOT.
+
  */
 
 #include "../readWD.hh"
@@ -31,7 +31,7 @@ int main(void)
     h2->SetLineColor(kGreen);
 
     file.Initialise(event);
-    while(file >> event)
+    while (file >> event)
     {
         auto pedestal = event.GetChannel(0, 0).GetPedestal();
         h1->Fill(pedestal.first);

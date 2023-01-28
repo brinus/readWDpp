69ab54e803801bc0e59c7098df50db8c854f74fe
brinus <brinus@users.noreply.github.com>
2023-01-28 16:13:05 +0000

    deploy: 879344784db6c0a0733cd3933bd9747abf2d4125

--- search/all_b.js
+++ search/all_b.js
@@ -3,5 +3,7 @@ var searchData=
   ['samples_5fper_5fwaveform_0',['SAMPLES_PER_WAVEFORM',['../readWD_8hh.html#a0606d74796559e9e9b17d890a305d35a',1,'readWD.hh']]],
   ['sec_1',['sec',['../structEventHeader.html#a54e689cf8c5e5aa0f749238c028ac106',1,'EventHeader']]],
   ['serialnumber_2',['serialNumber',['../structEventHeader.html#a0ec383672d74e1048054ebda0704fcde',1,'EventHeader']]],
-  ['setpedinterval_3',['SetPedInterval',['../classDAQEvent.html#ac5d33cf8009906f10396948b9934227c',1,'DAQEvent']]]
+  ['setintwindow_3',['SetIntWindow',['../classDAQEvent.html#acd1b3e8045de9fc782300bd80e808e59',1,'DAQEvent::SetIntWindow(int, int)'],['../classDAQEvent.html#ab8255f5f9bb63ea2abb114fdda5174a5',1,'DAQEvent::SetIntWindow(float, float)']]],
+  ['setpeakthr_4',['SetPeakThr',['../classDAQEvent.html#a698fb367ce23e6731f7f8613ed646a52',1,'DAQEvent']]],
+  ['setpedinterval_5',['SetPedInterval',['../classDAQEvent.html#ac5d33cf8009906f10396948b9934227c',1,'DAQEvent']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['setpedinterval_0',['SetPedInterval',['../classDAQEvent.html#ac5d33cf8009906f10396948b9934227c',1,'DAQEvent']]]
+  ['setintwindow_0',['SetIntWindow',['../classDAQEvent.html#acd1b3e8045de9fc782300bd80e808e59',1,'DAQEvent::SetIntWindow(int, int)'],['../classDAQEvent.html#ab8255f5f9bb63ea2abb114fdda5174a5',1,'DAQEvent::SetIntWindow(float, float)']]],
+  ['setpeakthr_1',['SetPeakThr',['../classDAQEvent.html#a698fb367ce23e6731f7f8613ed646a52',1,'DAQEvent']]],
+  ['setpedinterval_2',['SetPedInterval',['../classDAQEvent.html#ac5d33cf8009906f10396948b9934227c',1,'DAQEvent']]]
 ];

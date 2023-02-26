c13de7de116feace4925ce0f886fa32d3f426c22
brinus <brinus@users.noreply.github.com>
2023-02-26 18:04:11 +0000

    deploy: 135781e8aaf7b64e1526e4356af8b413b188946e

--- search/functions_4.js
+++ search/functions_4.js
@@ -6,8 +6,9 @@ var searchData=
   ['getintegrationbounds_3',['GetIntegrationBounds',['../classDAQEvent.html#a4a7f06bf5097c12cab47c52c11b1801e',1,'DAQEvent']]],
   ['getpeakindices_4',['GetPeakIndices',['../classDAQEvent.html#ab51c29021323fe4487db8d9e1d5237c4',1,'DAQEvent']]],
   ['getpedestal_5',['GetPedestal',['../classDAQEvent.html#a1f094a3b66812c85101669d298ae4d05',1,'DAQEvent']]],
-  ['gettime_6',['GetTime',['../classDAQEvent.html#a68d818ecf91c0d69fb2c7894f1ec532c',1,'DAQEvent']]],
-  ['gettimecf_7',['GetTimeCF',['../classDAQEvent.html#a5ec1cec99e28354bab26cb403b2bbb72',1,'DAQEvent']]],
-  ['gettimes_8',['GetTimes',['../classDAQEvent.html#a1300b318afa84d0c4a49c0ec63e5e56d',1,'DAQEvent']]],
-  ['getvolts_9',['GetVolts',['../classDAQEvent.html#a92675bb38933e12275a917d6b48357e5',1,'DAQEvent']]]
+  ['getrisetime_6',['GetRiseTime',['../classDAQEvent.html#a4276ac20ed45f5c494c26878779eb401',1,'DAQEvent']]],
+  ['gettime_7',['GetTime',['../classDAQEvent.html#a68d818ecf91c0d69fb2c7894f1ec532c',1,'DAQEvent']]],
+  ['gettimecf_8',['GetTimeCF',['../classDAQEvent.html#a5ec1cec99e28354bab26cb403b2bbb72',1,'DAQEvent']]],
+  ['gettimes_9',['GetTimes',['../classDAQEvent.html#a1300b318afa84d0c4a49c0ec63e5e56d',1,'DAQEvent']]],
+  ['getvolts_10',['GetVolts',['../classDAQEvent.html#a92675bb38933e12275a917d6b48357e5',1,'DAQEvent']]]
 ];

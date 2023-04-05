3f3d4edd59d024b9ff37baae46f25a93186c32c2
brinus <brinus@users.noreply.github.com>
2023-04-05 15:47:51 +0000

    deploy: 7cd76bc284e3362749dbe8f7d3b719c44c0aa10a

--- search/all_6.js
+++ search/all_6.js
@@ -7,7 +7,8 @@ var searchData=
   ['is_5fgetch_5f_4',['is_getch_',['../classDAQEvent.html#adb8d234709d6c85d19f2213d6e6664b6',1,'DAQEvent']]],
   ['is_5finit_5f_5',['is_init_',['../classDAQEvent.html#a27db83ed55f55bcf3fc7361bdfa9884f',1,'DAQEvent']]],
   ['is_5fiw_5f_6',['is_iw_',['../classDAQEvent.html#a0a6419e4262669022652474bc75f488c',1,'DAQEvent']]],
-  ['is_5fpeak_5f_7',['is_peak_',['../classDAQEvent.html#aea883f5ba05b1659236b1833d910630c',1,'DAQEvent']]],
-  ['is_5fped_5f_8',['is_ped_',['../classDAQEvent.html#a58907caf1b74b43e1f149ece458c7583',1,'DAQEvent']]],
-  ['iw_5f_9',['iw_',['../classDAQEvent.html#a1fc98902d13b6b09a272d384bd3b7fbb',1,'DAQEvent']]]
+  ['is_5flab_5f_7',['is_lab_',['../classDAQFile.html#a59d48fe9f25c3a0c8bcc2c239aa308b4',1,'DAQFile']]],
+  ['is_5fpeak_5f_8',['is_peak_',['../classDAQEvent.html#aea883f5ba05b1659236b1833d910630c',1,'DAQEvent']]],
+  ['is_5fped_5f_9',['is_ped_',['../classDAQEvent.html#a58907caf1b74b43e1f149ece458c7583',1,'DAQEvent']]],
+  ['iw_5f_10',['iw_',['../classDAQEvent.html#a1fc98902d13b6b09a272d384bd3b7fbb',1,'DAQEvent']]]
 ];

f22ee03923efb3e72f06672eb98a380a96660a27
brinus <brinus@users.noreply.github.com>
2023-04-12 15:45:45 +0000

    deploy: 52ae8e3393cb1d29660a39d9406f1f3a5e0ddbee

--- search/functions_4.js
+++ search/functions_4.js
@@ -3,12 +3,11 @@ var searchData=
   ['getamplitude_0',['GetAmplitude',['../classDAQEvent.html#afb33ea10753d01e87a8e0b3d1cd38d4c',1,'DAQEvent']]],
   ['getchannel_1',['GetChannel',['../classDAQEvent.html#ab1176184ce5206715972cb5fb965a8b9',1,'DAQEvent']]],
   ['getcharge_2',['GetCharge',['../classDAQEvent.html#a8d5626128e1c0aaa4844c9f2db941bc5',1,'DAQEvent']]],
-  ['getintegrationbounds_3',['GetIntegrationBounds',['../classDAQEvent.html#a4a7f06bf5097c12cab47c52c11b1801e',1,'DAQEvent']]],
-  ['getpeakindices_4',['GetPeakIndices',['../classDAQEvent.html#ab51c29021323fe4487db8d9e1d5237c4',1,'DAQEvent']]],
-  ['getpedestal_5',['GetPedestal',['../classDAQEvent.html#a1f094a3b66812c85101669d298ae4d05',1,'DAQEvent']]],
-  ['getrisetime_6',['GetRiseTime',['../classDAQEvent.html#a4276ac20ed45f5c494c26878779eb401',1,'DAQEvent']]],
-  ['gettime_7',['GetTime',['../classDAQEvent.html#a68d818ecf91c0d69fb2c7894f1ec532c',1,'DAQEvent']]],
-  ['gettimecf_8',['GetTimeCF',['../classDAQEvent.html#a5ec1cec99e28354bab26cb403b2bbb72',1,'DAQEvent']]],
-  ['gettimes_9',['GetTimes',['../classDAQEvent.html#a1300b318afa84d0c4a49c0ec63e5e56d',1,'DAQEvent']]],
-  ['getvolts_10',['GetVolts',['../classDAQEvent.html#a92675bb38933e12275a917d6b48357e5',1,'DAQEvent']]]
+  ['getpeakindices_3',['GetPeakIndices',['../classDAQEvent.html#ab51c29021323fe4487db8d9e1d5237c4',1,'DAQEvent']]],
+  ['getpedestal_4',['GetPedestal',['../classDAQEvent.html#a1f094a3b66812c85101669d298ae4d05',1,'DAQEvent']]],
+  ['getrisetime_5',['GetRiseTime',['../classDAQEvent.html#a4276ac20ed45f5c494c26878779eb401',1,'DAQEvent']]],
+  ['gettime_6',['GetTime',['../classDAQEvent.html#a68d818ecf91c0d69fb2c7894f1ec532c',1,'DAQEvent']]],
+  ['gettimecf_7',['GetTimeCF',['../classDAQEvent.html#a5ec1cec99e28354bab26cb403b2bbb72',1,'DAQEvent']]],
+  ['gettimes_8',['GetTimes',['../classDAQEvent.html#a1300b318afa84d0c4a49c0ec63e5e56d',1,'DAQEvent']]],
+  ['getvolts_9',['GetVolts',['../classDAQEvent.html#a92675bb38933e12275a917d6b48357e5',1,'DAQEvent']]]
 ];
--- search/functions_9.js
+++ search/functions_9.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['setintwindow_0',['SetIntWindow',['../classDAQEvent.html#acd1b3e8045de9fc782300bd80e808e59',1,'DAQEvent::SetIntWindow(int, int)'],['../classDAQEvent.html#ab8255f5f9bb63ea2abb114fdda5174a5',1,'DAQEvent::SetIntWindow(float, float)']]],
-  ['setpeakthr_1',['SetPeakThr',['../classDAQEvent.html#a698fb367ce23e6731f7f8613ed646a52',1,'DAQEvent']]],
-  ['setpedinterval_2',['SetPedInterval',['../classDAQEvent.html#ac5d33cf8009906f10396948b9934227c',1,'DAQEvent']]]
+  ['setintwindow_0',['SetIntWindow',['../classDAQEvent.html#aae14678619562b01b8469eb415b53eb4',1,'DAQEvent::SetIntWindow(int, int)'],['../classDAQEvent.html#aa207b913538e3883af8dd8f26abd0c0a',1,'DAQEvent::SetIntWindow(float, float)']]],
+  ['setpeakthr_1',['SetPeakThr',['../classDAQEvent.html#a027d6a5b99eccf0d01cda8613aa4d46f',1,'DAQEvent']]],
+  ['setpedinterval_2',['SetPedInterval',['../classDAQEvent.html#a0ba141370d89ff2e417adea861f23895',1,'DAQEvent']]]
 ];

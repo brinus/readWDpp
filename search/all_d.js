cb14fe1fda9409ca715446fa74c14e4c941ad33b
brinus <brinus@users.noreply.github.com>
2023-04-14 10:25:39 +0000

    deploy: 3888bc0557e0d3a8a76bff5ae952a73859b9ca2e

--- search/all_d.js
+++ search/all_d.js
@@ -4,5 +4,6 @@ var searchData=
   ['tag_1',['tag',['../structTAG.html#a073fcb689b59f52b23c3ea874ca473ef',1,'TAG::tag()'],['../structEventHeader.html#a579ec852bf31d93558596ff9f45f6ebd',1,'EventHeader::tag()']]],
   ['the_20binary_20output_2',['The Binary Output',['../binary.html',1,'']]],
   ['timecalibration_3',['TimeCalibration',['../classDAQEvent.html#a1df88166e2ee2c22662c870d3a6463b4',1,'DAQEvent']]],
-  ['times_5f_4',['times_',['../classDAQEvent.html#aed4f7042be0939c874ea26377eb95e4e',1,'DAQEvent::times_()'],['../classDAQFile.html#a9725911cab2d6d9522cb5289833eea9b',1,'DAQFile::times_()']]]
+  ['times_5f_4',['times_',['../classDAQEvent.html#aed4f7042be0939c874ea26377eb95e4e',1,'DAQEvent::times_()'],['../classDAQFile.html#a9725911cab2d6d9522cb5289833eea9b',1,'DAQFile::times_()']]],
+  ['type_5f_5',['type_',['../classDAQFile.html#a1abd233280824677d01721b8cfc7231e',1,'DAQFile']]]
 ];
